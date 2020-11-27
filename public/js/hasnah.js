4ddce807acfd693a89fd0c13cf93f51187bffcb9
haxorsprogramming <haxorsuinsu@gmail.com>
2020-11-28 03:40:57 +0700

    update

--- public/js/hasnah.js
+++ public/js/hasnah.js
@@ -2,7 +2,9 @@ var divUtama = new Vue({
   el : '#divUtama',
   data : {
     blog : [
-      {title : 'Ayok sadar diri', kategori : 'blog', tanggal : 'Agustus 18 2020'}
+      {title : 'Ayok sadar diri', kategori : 'blog', tanggal : 'Agustus 2020'},
+      {title : 'Bahagianya minum aqua', kategori : 'blog', tanggal : 'September 2020'},
+      {title : 'Lagu itu 2020.10.28 rilis', kategori : 'blog', tanggal : 'Oktober 2020'}
     ],
     nama : 'Hasnah Nur Ardita'
   }
