bc2d6a879e65d583b8b5d3541316649d2eb795b3
haxorsprogramming <haxorsuinsu@gmail.com>
2020-11-28 01:48:06 +0700

    update

---
+++ public/js/hasnah.js
@@ -0,0 +1,15 @@
+var divUtama = new Vue({
+  el : '#divUtama',
+  data : {
+    blog : [
+      {title : 'Ayok sadar diri', kategori : 'blog', tanggal : 'Agustus 18 2020'}
+    ],
+    nama : 'Hasnah Nur Ardita'
+  }
+});
+
+
+function initFunc()
+{
+
+}
